462ae45f953495483f41e0431bfed47a8045ab83
diegonms <diegonms06@gmail.com>
2025-03-18 19:55:33 -0300

    atualizando

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.addEventListener("DOMContentLoaded", function () {
     height: 128,
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  menuItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.transform = "scale(1.05)";
+      item.style.transition = "0.3s";
+    });
+    item.addEventListener("mouseout", () => {
+      item.style.transform = "scale(1)";
+    });
+  });
+
+  const orderButton = document.querySelector(".order-button");
+  orderButton.addEventListener("mouseover", () => {
+    orderButton.style.backgroundColor = "#ff9800";
+    orderButton.style.color = "black";
+    orderButton.style.transition = "0.3s";
+  });
+  orderButton.addEventListener("mouseout", () => {
+    orderButton.style.backgroundColor = "black";
+    orderButton.style.color = "#E87722";
+  });
+});
